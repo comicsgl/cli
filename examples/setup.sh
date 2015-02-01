1a7777ff11e7f5244c7051b340dcc811aabb122c
Makis Tracend <makis.tracend@gmail.com>
2015-01-31 21:08:50 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the ComicsGL CLI
+
+# 1. Install the main executable
+npm install -g comicsgl-cli
+
+#2. Setup comicsgl (only needed once)
+comicsgl setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+comicsgl -h
